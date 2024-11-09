ec767899d9b3d33389403c1f9f7153891717fa4c
Fahma Rosyidah <fahmarosyi@gmail.com>
2024-11-09 16:36:14 +0700

    feat: login, _app & _document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { Box, ChakraProvider, HStack } from "@chakra-ui/react"
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import { AppProps } from "next/app"
+import { useRouter } from "next/router"
+import LoginGuard from "../components/LoginGuard"
+import theme from "../theme"
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  const isHome = router.pathname === "/"
+  return (
+    <ChakraProvider theme={theme}>
+        {isHome ? (
+          <Component {...pageProps} />
+        ) : (
+          <LoginGuard>
+            <HStack align="flex-start" spacing={4} h="100vh">
+              <Box flex={4} overflow="auto" p={10} bgColor="gray.100" h="full">
+                <Component {...pageProps} />
+              </Box>
+            </HStack>
+          </LoginGuard>
+        )}
+    </ChakraProvider>
+  )
 }
+
+export default MyApp
