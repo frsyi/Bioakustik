d9f602012ad933a6558c01c780141da0fc848f51
frsyi <fahmarosyi@gmail.com>
2025-02-02 14:38:08 +0700

    hook: useUser

---
+++ src/hooks/useUser.ts
@@ -0,0 +1,28 @@
+import useSWR from "swr"
+import useAuth from "./useAuth"
+
+export type User = {
+    id: string
+    name: string
+    username: string
+    role: string
+    _count: {
+        Audio: number
+    }
+}
+
+const useUser = () => {
+    const { fetcher } = useAuth()
+    const url = `/user`
+    const { data, ...swr } = useSWR(url, (url: string) =>
+        fetcher()
+            .get<{ data: User[] }>(url)
+            .then(({ data }) => data.data)
+    )
+    return {
+        data: data || [],
+        ...swr
+    }
+}
+
+export default useUser
