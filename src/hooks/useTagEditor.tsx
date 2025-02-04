5ad2ff27fe8505583e8576785e3c587b264e8787
frsyi <fahmarosyi@gmail.com>
2025-02-04 07:42:19 +0700

    hook: useTagEditor

---
+++ src/hooks/useTagEditor.tsx
@@ -0,0 +1,151 @@
+import {
+  Alert,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Select,
+  useDisclosure,
+} from "@chakra-ui/react"
+import { AxiosError } from "axios"
+import { createContext, useContext, useState } from "react"
+import useSWR from "swr"
+import useAuth from "./useAuth"
+import { useTag } from "./useTag"
+
+const ctx = createContext(null)
+
+export const useTagEditor = () => {
+  return useContext<{
+    handleOpen: (props: EditorBody) => void
+  }>(ctx)
+}
+
+type EditorBody = {
+  id: string
+  name: string
+  color: string
+  parentId: string | null
+}
+
+export const TagEditorProvider = ({ children }) => {
+  const { mutate } = useTag()
+  const defaultBody = { id: "", name: "", color: "#000000", parentId: "" }
+  const [body, setBody] = useState<EditorBody>(defaultBody)
+
+  const [error, setError] = useState("")
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { fetcher } = useAuth()
+  const { data } = useSWR(body.id, async (id) => {
+    if (id)
+      return fetcher()
+        .get<{ id: string; name: string; color: string }[]>(
+          `/tag/${id}/possible-parent`
+        )
+        .then(({ data }) => data)
+    return Promise.resolve([])
+  })
+  const tags = data?.length ? data : []
+
+  const handleOpen = (props: EditorBody) => {
+    setBody({ ...props })
+    setError("")
+    onOpen()
+  }
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    fetcher()
+      .patch(`/tag/${body.id}`, body)
+      .then(() => {
+        onClose()
+      })
+      .catch((err) => {
+        if (err instanceof AxiosError)
+          return setError(err?.response?.data?.message)
+        setError(err.message)
+      })
+      .finally(() => {
+        mutate()
+      })
+  }
+
+  return (
+    <ctx.Provider value={{ handleOpen }}>
+      {children}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Edit Tag</ModalHeader>
+          <ModalBody>
+            {error && (
+              <Alert status="error" my={4}>
+                {error}
+              </Alert>
+            )}
+            <form onSubmit={handleSubmit}>
+              <Flex flexDir="column" gap={3}>
+                <FormControl>
+                  <FormLabel>Parent</FormLabel>
+                  <Select
+                    value={body.parentId || ""}
+                    onChange={(e) => {
+                      if (e.target.value === "") {
+                        setBody({ ...body, parentId: null })
+                        return
+                      }
+                      setBody({ ...body, parentId: e.target.value })
+                    }}
+                  >
+                    <option value="">None</option>
+                    {tags?.map((tag) => (
+                      <option key={tag.id} value={tag.id}>
+                        {tag.name}
+                      </option>
+                    ))}
+                  </Select>
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Name</FormLabel>
+                  <Input
+                    type="text"
+                    placeholder="Name"
+                    value={body.name}
+                    onChange={(e) => setBody({ ...body, name: e.target.value })}
+                  />
+                </FormControl>
+                <Input
+                  type="color"
+                  placeholder="Color"
+                  value={body.color}
+                  width="50px"
+                  aspectRatio={1}
+                  border="none"
+                  borderRadius="50%"
+                  p={0}
+                  onChange={(e) => setBody({ ...body, color: e.target.value })}
+                />
+                <Button
+                  type="submit"
+                  bgColor={"purple"}
+                  color="white"
+                  _hover={{ opacity: 0.8 }}
+                >
+                  Save
+                </Button>
+              </Flex>
+            </form>
+          </ModalBody>
+          <ModalFooter />
+        </ModalContent>
+      </Modal>
+    </ctx.Provider>
+  )
+}
